64194154b7c47c1929c04578e0785fcd57e5984a
sumit mandloi <sumitkumar77.sk@gmail.com>
2023-04-28 12:57:56 +0530

    v1.1

--- lib/utilities/app_constants.dart
+++ lib/utilities/app_constants.dart
@@ -3,7 +3,7 @@ abstract class AppConstants {
   static const appTitle = 'Sumit Mandloi';
   static const disclaimer =
       'I do not claim ownership of the projects below as some of them were developed for companies I worked for and some for clients/clients of my clients while the backend side is provided.';
-  static const showcaseTitle = 'SHOWCASE';
+  static const showcaseTitle = 'SHOWCASE ';
   static const showcaseDescription =
       'MOBILE APPLICATIONS EITHER DEVELOPED COMPLETELY BY ME OR BY A TEAM WHERE I PARTICIPATED SIGNIFICANTLY';
   static const landingTitle = 'SUMIT MANDLOI';
