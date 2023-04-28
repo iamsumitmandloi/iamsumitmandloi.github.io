a3dcecb781f17ef83dd6d7f11c911759b6c0101c
sumit mandloi <sumitkumar77.sk@gmail.com>
2023-04-28 13:14:10 +0530

    live

--- README.md
+++
@@ -1,6 +0,0 @@
-# Portfolio website using Flutter Web
-
-A responsive layout; open-source, comment-rich, and clean codebase website built with Flutter web.
-
-
-
--- lib/utilities/asset_const.dart
+++
@@ -1,5 +0,0 @@
-class AssetConstant {
-  static const ihlc = 'assets/images/ihlc.jpg';
-  static const ihlp = 'assets/images/ihlp.png';
-  static const investmates = 'assets/images/investmates.jpg';
-}
